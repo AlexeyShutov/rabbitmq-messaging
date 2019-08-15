5c01f66e1c762a223b8fdb76d76bf0fe99d872e7
AlexeyShutov <alexey.sh91@gmail.com>
2019-08-15 17:30:08 +0200

    Docker compose RabbitMQ cluster config; initial config for producer-consumer applications

---
+++ consumer/src/main/java/com/messaging/consumer/ConsumerApp.java
@@ -0,0 +1,13 @@
+package com.messaging.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApp.class, args);
+    }
+
+}
---
+++ producer/build.gradle.kts
@@ -0,0 +1,33 @@
+val springBootVersion = "2.1.7.RELEASE"
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.springframework.boot:spring-boot-gradle-plugin:2.1.7.RELEASE")
+    }
+}
+
+plugins {
+    java
+    id("org.springframework.boot") version "2.1.7.RELEASE"
+}
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_10
+    targetCompatibility = JavaVersion.VERSION_1_10
+}
+
+springBoot {
+    mainClassName = "com.messaging.producer.ProducerApp"
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-parent:$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-amqp:$springBootVersion")
+}
